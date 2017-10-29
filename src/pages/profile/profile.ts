563e3f6bbcc597db17284230fd519d6fff258ddc
c-silber <csilber@ramapo.edu>
2017-10-29 17:49:03 -0400

    testing cordova

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -50,30 +50,59 @@ export class ProfilePage {
 
     }
 
+    public takePicture(sourceType) {
+        // Create options for the Camera Dialog
+        var options = {
+          quality: 100,
+          sourceType: sourceType,
+          saveToPhotoAlbum: false,
+          correctOrientation: true
+        };
+
+        // Get the data of an image
+        this.camera.getPicture(options).then((imagePath) => {
+          // Special handling for Android library
+          if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+            this.filePath.resolveNativePath(imagePath)
+              .then(filePath => {
+                let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+                let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+                this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+              });
+          } else {
+            var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+            var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          }
+        }, (err) => {
+          this.presentToast('Error while selecting image.');
+        });
+      }
+
     public presentActionSheet() {
-    let actionSheet = this.actionSheetCtrl.create({
-      title: 'Select Image Source',
-      buttons: [
-        {
-          text: 'Load from Library',
-          handler: () => {
-            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-          }
-        },
-        {
-          text: 'Use Camera',
-          handler: () => {
-            this.takePicture(this.camera.PictureSourceType.CAMERA);
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    });
-    actionSheet.present();
-  }
+        let actionSheet = this.actionSheetCtrl.create({
+          title: 'Select Image Source',
+          buttons: [
+            {
+              text: 'Load from Library',
+              handler: () => {
+                this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+              }
+            },
+            {
+              text: 'Use Camera',
+              handler: () => {
+                this.takePicture(this.camera.PictureSourceType.CAMERA);
+              }
+            },
+            {
+              text: 'Cancel',
+              role: 'cancel'
+            }
+          ]
+        });
+        actionSheet.present();
+      }
 
   public uploadImage() {
       // Destination URL
