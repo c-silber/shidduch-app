db66d4b67d796ef882f233b44ed92b3701f10603
c-silber <csilber@ramapo.edu>
2017-10-30 21:03:58 -0400

    firebase

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase : {
-    apiKey: "AIzaSyC-yi2ZBpGPJP1Xi-f5YFEEpRonMf-Lt8w",
-    authDomain: "shidduch-line.firebaseapp.com",
-    databaseURL: "https://shidduch-line.firebaseio.com",
-    projectId: "shidduch-line",
-    storageBucket: "shidduch-line.appspot.com",
-    messagingSenderId: "692829068022"
+    apiKey: "xxxxxxxx-xxxx-xxxx",
+    authDomain: "xxxxxxxx-xxxx-xxxx",
+    databaseURL: "xxxxxxxx-xxxx-xxxx",
+    projectId: "xxxxxxxx-xxxx-xxxx",
+    storageBucket: "xxxxxxxx-xxxx-xxxx",
+    messagingSenderId: "xxxxxxxx-xxxx-xxxx"
   }
 };
